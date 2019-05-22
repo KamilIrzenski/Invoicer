9445afa00fab3910c91ab62640bca3aa3762b3d5
Kamil Irzenski <kirzenski@gmail.com>
2019-05-22 18:30:27 +0200

    Add new class Product

---
+++ Invoiicer/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoiicer.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
